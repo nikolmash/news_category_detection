6e70f54c7d95e9bbd7715670ea3af9b69437ed2c
Maria <31651184+nikolmash@users.noreply.github.com>
2020-06-27 03:54:44 +0300

    Add files via upload

---
+++ preprocessing.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+import re
+import nltk
+import pymorphy2
+from string import punctuation
+from nltk.corpus import stopwords
+
+
+class TextPreprocessing:
+    def __init__(self, text, is_stopwords, 
+                 is_lemmatization):
+        self.text = text
+        self.is_stopwords = is_stopwords
+        self.is_lemmatization = is_lemmatization
+        self.morph = pymorphy2.MorphAnalyzer()
+        self.stopwords = set(stopwords.words('russian'))
+        self.punkt = punctuation + '«»—…“”*№–'
+    
+    def preprocess(self):
+        clean_text = ' '.join([word.strip(self.punkt) for word in self.text.lower().split()])
+        if self.is_stopwords == 1:
+            clean_text = ' '.join([word for word in clean_text.split() if word not in self.stopwords])
+        if self.is_lemmatization == 1:
+            clean_text = ' '.join([self.morph.parse(word)[0].normal_form for word in clean_text.split()])
+        return clean_text
+
