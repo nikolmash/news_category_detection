c1815db731649a5e8a394e3967c4cb438da38377
Maria <31651184+nikolmash@users.noreply.github.com>
2020-06-27 14:44:46 +0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -13,17 +13,16 @@ from nltk.corpus import stopwords
 
 
 class TextPreprocessing:
-    def __init__(self, text, is_stopwords, 
+    def __init__(self, is_stopwords, 
                  is_lemmatization):
-        self.text = text
         self.is_stopwords = is_stopwords
         self.is_lemmatization = is_lemmatization
         self.morph = pymorphy2.MorphAnalyzer()
         self.stopwords = set(stopwords.words('russian'))
         self.punkt = punctuation + '«»—…“”*№–'
     
-    def preprocess(self):
-        clean_text = ' '.join([word.strip(self.punkt) for word in self.text.lower().split()])
+    def preprocess(self, text):
+        clean_text = ' '.join([word.strip(self.punkt) for word in text.lower().split()])
         if self.is_stopwords == 1:
             clean_text = ' '.join([word for word in clean_text.split() if word not in self.stopwords])
         if self.is_lemmatization == 1:
