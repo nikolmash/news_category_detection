c1825725068ac2c87290cb6ac141a9d07b04a90c
psaleksandrova <psaleksandrova@edu.hse.ru>
2020-06-28 14:14:35 +0300

    fix force download

--- get_models.py
+++ get_models.py
@@ -1,15 +1,22 @@
 import wget
 import os
-
+import logging
 from joblib import load
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s @ %(message)s",
+    datefmt="%d-%m-%Y %H:%M:%S",
+)
+logger = logging.getLogger(name="ModelDownloader")
+
 TF_IDF_LINKS = ["https://www.dropbox.com/s/jepxzzyd982ef3n/tfidf_0_0.pkl?dl=1",
-                "https://www.dropbox.com/s/q8rsynwc8arv3bv/tfidf_0_1.pkl?dl=1",
                 "https://www.dropbox.com/s/73m7wqvi9q8gb1j/tfidf_1_0.pkl?dl=1",
+                "https://www.dropbox.com/s/q8rsynwc8arv3bv/tfidf_0_1.pkl?dl=1",
                 "https://www.dropbox.com/s/y6chuopdmp1ug8z/tfidf_1_1.pkl?dl=1"]
 MODEL_LINKS = ["https://www.dropbox.com/s/ev6ylo08fv0g70h/lr_0_0.joblib?dl=1",
-               "https://www.dropbox.com/s/5g0fbhqj4bsxedj/lr_0_1.joblib?dl=1"
                "https://www.dropbox.com/s/01wzyc7wtifyezl/lr_1_0.joblib?dl=1",
+               "https://www.dropbox.com/s/5g0fbhqj4bsxedj/lr_0_1.joblib?dl=1",
                "https://www.dropbox.com/s/zuxzlhrg1do46fb/lr_1_1.joblib?dl=1"]
 
 
@@ -34,8 +41,14 @@ def force_download():
         for lemmatize in range(2):
             if not os.path.exists('./lr_%d_%d.joblib' % (stop_words, lemmatize)):
                 download_model(stop_words, lemmatize, MODEL_LINKS)
+                logger.info(f"lr_%d_%d.joblib was downloaded." % (stop_words, lemmatize))
+            else:
+                logger.info(f"lr_%d_%d.joblib already exists." % (stop_words, lemmatize))
             if not os.path.exists('./tfidf_%d_%d.pkl' % (stop_words, lemmatize)):
                 download_model(stop_words, lemmatize, TF_IDF_LINKS)
+                logger.info(f"tfidf_%d_%d.pkl was downloaded." % (stop_words, lemmatize))
+            else:
+                logger.info(f"tfidf_%d_%d.pkl already exists." % (stop_words, lemmatize))
 
 
 if __name__ == '__main__':
