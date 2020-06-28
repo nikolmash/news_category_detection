93f84bbadc9af6f79d152a630830dd19c097022f
Maria <31651184+nikolmash@users.noreply.github.com>
2020-06-28 20:43:52 +0300

    update model and tfidf links

--- get_models.py
+++ get_models.py
@@ -11,12 +11,12 @@ logging.basicConfig(
 logger = logging.getLogger(name="ModelDownloader")
 
 TF_IDF_LINKS = ["https://www.dropbox.com/s/jepxzzyd982ef3n/tfidf_0_0.pkl?dl=1",
-                "https://www.dropbox.com/s/73m7wqvi9q8gb1j/tfidf_1_0.pkl?dl=1",
+                "https://www.dropbox.com/s/namig2jwslatn4h/tfidf_0_1.pkl?dl=1",
                 "https://www.dropbox.com/s/q8rsynwc8arv3bv/tfidf_0_1.pkl?dl=1",
                 "https://www.dropbox.com/s/y6chuopdmp1ug8z/tfidf_1_1.pkl?dl=1"]
 MODEL_LINKS = ["https://www.dropbox.com/s/ev6ylo08fv0g70h/lr_0_0.joblib?dl=1",
                "https://www.dropbox.com/s/01wzyc7wtifyezl/lr_1_0.joblib?dl=1",
-               "https://www.dropbox.com/s/5g0fbhqj4bsxedj/lr_0_1.joblib?dl=1",
+               "https://www.dropbox.com/s/0uh0vc1x7rzpi5u/lr_0_1.joblib?dl=1",
                "https://www.dropbox.com/s/zuxzlhrg1do46fb/lr_1_1.joblib?dl=1"]
 
 
